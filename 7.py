54b4dd97527635d5831caf2b21ca22d9a2cf1baf
Siddharth Janghu <siddharthj.dce@gmail.com>
2017-06-25 14:48:26 +1000

    Codeacademy chap

---
+++ 7.py
@@ -0,0 +1,17 @@
+x = "There are %d type of people" % 10
+binary = "binary"
+do_not = "don't"
+y ="Those who know %s and those who %s" % (binary, do_not)
+
+print x
+print y
+
+funny = False
+jokeevaluaate = "Isn't that joke so funny? %r"
+
+print jokeevaluaate % funny
+
+w = "This is left and"
+e = "Thise is also left"
+
+print w + e
